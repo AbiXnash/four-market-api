310cd1b60260dec1660f14888dda1b95409007a5
Abinash <abinash@theabx.in>
2026-05-26 23:36:15 +0530

    fix: CSRF token check issue

--- src/main/kotlin/in/abx/auth/v1/controller/AuthController.kt
+++ src/main/kotlin/in/abx/auth/v1/controller/AuthController.kt
@@ -6,10 +6,7 @@ import `in`.abx.auth.service.LoginService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -21,7 +18,7 @@ class AuthController(
             LoggerFactory.getLogger(AuthController::class.java)
     }
 
-    @PostMapping("/csrf")
+    @GetMapping("/csrf")
     fun csrfRoute(): ResponseEntity<Void> {
         return ResponseEntity.ok().build()
     }
