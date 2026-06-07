40986da07ed53554272de27487c08522c68f3972
Abinash <abinash@theabx.in>
2026-06-07 21:10:12 +0530

    refactor(middleware): remove unused parameter from RequireHeaders

--- internal/middleware/required_header.go
+++ internal/middleware/required_header.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RequireHeaders(requireHeaders ...string) gin.HandlerFunc {
+func RequireHeaders() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if headerValue := c.GetHeader("Client-App"); headerValue != "4M" {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
